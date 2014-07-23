cf985a31e758d4f70f4c572ef41c74d6efab1d24
ialberdi <ialberdi@PFC10.vicomtech>
2014-07-23 11:14:24 +0200

    add new fuctionalities

--- js/main.js
+++ js/main.js
@@ -240,6 +240,8 @@ var app = {
 	$('#javascrip .userProximity').append('<br><div>userProximity</div><br><div>'+userProximity+'</div>');
 	$('#javascrip .deviceProximity').append('<br><div>deviceProximity</div><br><div>'+deviceProximity+'</div>');
 
+	console.log($('body'));
+
 	/*console.log(discovery.screenSize());
 	console.log(discovery.geolocation());
 	console.log(discovery.orientation());
